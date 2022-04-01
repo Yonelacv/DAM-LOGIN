fb4caafd4aaff83a96917577c0fea6354bf7378d
Yonela Carbajal <yonela.carbajal@tecsup.edu.pe>
2022-04-01 00:10:14 -0500

    login

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,35 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { Alert, Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { Image } from 'react-native';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
+    
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Login</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Image style={styles.imgStyle}
+      source={ require('./images/usuario.png') }
+      ></Image>
+      <Text>Introduce tu email:</Text>
+      <TextInput 
+          placeholder="Email" />
+      <Text>Introduce tu contraseña:</Text>
+      <TextInput
+          secureTextEntry={true}
+          placeholder="Password"
+      />
+        <TouchableOpacity
+        style={styles.button}
+        onPress={onPress}
+      >
+        <Text>Iniciar Sesión</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -29,4 +49,16 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+
+  imgStyle: {
+    width: 100,
+    height: 100,
+    marginTop: 10,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+  
 });
